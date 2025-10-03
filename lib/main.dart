5041ef4f9f926dd51be922c3acf777e3d8f13fbe
CentyGG <makarsyshchikov04@gmail.com>
2025-10-03 13:38:54 +0300

    Эксперименты с widget Row

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('Сыщиков Макар Максимович ИКБО-06-22 22И1809' ,
