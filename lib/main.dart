954618472c2629f60de4e604edd88b99b00a3112
CentyGG <makarsyshchikov04@gmail.com>
2025-10-03 19:01:40 +0300

    Сделана игра кликер

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter clicker game',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -24,44 +24,131 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage> {
+  var clicks = 0;
+  var firstboss_clicks = 0;
+  var secondboss_clicks = 0;
+  var currentBoss = 1;
+  var firstBossHealth = 30;
+  var secondBossHealth = 150;
+  int get boss_clicks => currentBoss == 1 ? firstboss_clicks : secondboss_clicks;
+  int get currentBossHealth => currentBoss == 1 ? firstBossHealth : secondBossHealth;
+  int get remainingHealth => currentBossHealth - clicks;
+  void onTap() {
+    setState(() {
+      clicks++;
+      if (currentBoss == 1) {
+        firstboss_clicks++;
+      } else {
+        secondboss_clicks++;
+      }
+    });
+  }
+  void resetGame() {
+    setState(() {
+      clicks = 0;
+      firstboss_clicks = 0;
+      secondboss_clicks = 0;
+      firstBossHealth = 30;
+      secondBossHealth = 150;
+    });
+  }
+  void selectFirstBoss() {
+    setState(() {
+      resetGame();
+      currentBoss = 1;
+    });
+  }
+  void selectSecondBoss() {
+    setState(() {
+      resetGame();
+      currentBoss = 2;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    final isBossDefeated = remainingHealth <= 0;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(50)
-            ),
-                padding: EdgeInsets.all(30),
-                child: const Text('Сыщиков Макар Максимович ИКБО-06-22 22И1809' ,
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.green,
-                        fontStyle: FontStyle.italic
-                    ))
+            const Text(
+              'Выбери босса',
+              style: TextStyle(
+                color: Colors.black
+              ),
+              textScaler: TextScaler.linear(3),
             )
           ,
-            ElevatedButton(
-              onPressed: null,
-              child: const Text(
-                'Пример',
-               style: TextStyle(color: Colors.black),
-              ),
-              style: ElevatedButton.styleFrom(
+          Padding(padding: EdgeInsets.all(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed:selectFirstBoss,
+                  style: ElevatedButton.styleFrom(
+                      elevation: currentBoss == 1 ? 10 : 0,
+                      backgroundColor: Colors.white,
+                      disabledBackgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                          side: BorderSide.none
+                    )
+                  ),
+                  child: Image.asset('assets/images/new_1.jpg',width: 100, height: 140)),
+                Padding(padding: EdgeInsets.all(50)),
+                ElevatedButton(
+                    onPressed: selectSecondBoss,
+                    style: ElevatedButton.styleFrom(
+                        elevation: currentBoss == 2 ? 10 : 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero,
+                          side: BorderSide.none
+                        ),
+                        backgroundColor: Colors.white,
+                        disabledBackgroundColor: Colors.white
+                    ),
+                    child: Image.asset('assets/images/new_2.jpg',width: 70, height: 140)),
+              ]
 
-                disabledBackgroundColor: Colors.deepOrange,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50)
-                )
-            )
-        )]
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Text(
+              isBossDefeated
+                  ? 'БОСС ПОВЕРЖЕН!'
+                  : 'Осталось здоровья: $remainingHealth',
+              style: TextStyle(
+                color: isBossDefeated ? Colors.red : Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              textScaler: const TextScaler.linear(2),
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Image.asset(
+                currentBoss == 1
+                    ? (remainingHealth <= 0 ? 'assets/images/defeated_1.jpg' : 'assets/images/new_1.jpg')
+                    : (remainingHealth <= 0 ? 'assets/images/defeated_2.jpg' : 'assets/images/new_2.jpg'),
+                width: 100,
+                height: 240
+            ),
+            Padding(padding: EdgeInsets.all(30)),
+            ElevatedButton(
+              onPressed: isBossDefeated ? null : onTap,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isBossDefeated ? Colors.grey : Colors.blue,
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              child: Text(
+                isBossDefeated ? 'ПОБЕДА!' : 'Кликнуть',
+                textScaler: const TextScaler.linear(2),
+                style: const TextStyle(color: Colors.white),
+              ))]
         )
       ));
   }
